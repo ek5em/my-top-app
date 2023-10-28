7e2083d2852ea3ebb4516508e4a61a409c5c59d4
ek5em <pro-100-dim@mail.ru>
2023-10-28 20:23:56 +0400

    Шрифты

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,30 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Head from "next/head";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'My top app',
-}
+   title: "My top app",
+};
 
 export default function RootLayout({
-  children,
+   children,
 }: {
-  children: React.ReactNode
+   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+   return (
+      <html lang="ru">
+         <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link
+               href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap"
+               rel="stylesheet"
+            />
+         </Head>
+         <body className={inter.className}>{children}</body>
+      </html>
+   );
 }
